52c0f9553e237cd634470bca8a43f8223d55cdc1
fguaita <francisco.guaita@soprasteria.com>
2018-02-12 12:23:48 +0100

    Added signin component. Also modified auth service to sign up a user

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -9,8 +9,24 @@ constructor(private router: Router){}
 
     signupUser(email:string, password:string){
         firebase.auth().createUserWithEmailAndPassword(email, password)
-            .catch(error => console.error('signupUser error: ', error));
-        this.router.navigate(['/']);
+            .then(response => {
+                alert('User created successfully');
+                this.router.navigate(['/']);
+            })
+            .catch(error => 
+            {
+                alert(error.message);
+            })
     }
 
+    signinUser(email:string, password:string){
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(response => {
+                this.router.navigate(['/']);
+            })
+            .catch(error => 
+            {
+                alert(error.message);
+            })
+    }
 }
